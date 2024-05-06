1e829153b3aae9a9ee1362ae2b6cbd761a7758d1
adil Zeb <adilzeb776@gmail.com>
2024-05-06 12:13:24 +0500

    Basic Js Code

---
+++ Variables_DataTypes.js
@@ -0,0 +1,40 @@
+// Values
+/*1) Values Are those data that is assign to the  variables
+2) Probably the data which is Right Side to the Assignment Operator that is Value 
+ Values is either object or Premitive Vale
+*/
+//Premitive Value
+let firstName = "Adil Zeb";
+console.log(firstName); // Adil Zeb Is a value
+//Object
+let Profile = {
+  name: "Adil",
+  age: 23,
+};
+
+console.log(Profile.name + Profile.age);
+
+//Variables
+/* Those Data whose take Some Values We say Variables, or usually in the left side of the Assignment operator */
+
+let Data = "Js Programing Language";
+console.log(Data); //Data is Variable in line 10
+
+//Data Types
+/* There Are 7 Data types in Js 
+1) Number
+2) Boolean
+3)String
+4)Undefined
+5)Null
+6) Symbol
+7)Big Int
+ */
+
+let name = 23; //Number
+let fullage = true; //Boolean
+let Str = "abc"; //String
+let Undefined; //Undefined
+let Null; //Null
+let value = Symbol("#"); //Symbol
+let bigdata = "12345678987654322345679876"; //Big Int
