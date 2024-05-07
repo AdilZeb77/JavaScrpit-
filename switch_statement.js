09d033691118dd5de62c1853e6854bb3fa887ae3
adil Zeb <adilzeb776@gmail.com>
2024-05-07 11:24:21 +0500

    weekly todo using switch_Statement

---
+++ switch_statement.js
@@ -0,0 +1,25 @@
+const day = "monday";
+
+switch (day) {
+  case "monday":
+    console.log("Watching Video Lactures");
+    break;
+  case "tuesday":
+    console.log("Practicing lacture");
+    break;
+  case "wednesday":
+    console.log("Writing code");
+    break;
+  case "thursday":
+    console.log("Giving Presentation");
+    break;
+  case "friday":
+    console.log(" Giving Final Demo");
+    break;
+  case "saturday":
+    console.log(" Dusting And Cleaning Home  ");
+    break;
+  case "sunday":
+    console.log("wow its Day of Chill");
+    break;
+}
