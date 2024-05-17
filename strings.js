938083857543d21d3f0903026a6d105cad42ce0e
adil Zeb <adilzeb776@gmail.com>
2024-05-17 16:29:40 +0500

    Strings With Diff Methods

--- strings.js
+++ strings.js
@@ -45,7 +45,7 @@ console.log(hotelSeat.startsWith('1'));
 
  console.log(hotelSeat.trim());
 
- 
+
 
 
 //function to check weather a seat is front middle or corner
@@ -74,5 +74,16 @@ seat('R1M');
 seat('R1C');
 
 
+// String Coading Exercise 
+
+const flights ='_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 
+ const getCode=str => str.slice(0,3).toLocaleUpperCase();
+for (const flight of flights.split("+")){
+ const [type, from , to , time]=flight.split(";");
+
+ const output =  ` ${type.startsWith("_Delayed" ) ? '🤕' :' '}${type.replaceAll("_"," ")} ${getCode(from)} ${getCode(to)} ${time.replace(":","h")}`;
+
+console.log(output);
+}
