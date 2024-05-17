7abd518647c4557800ded0d80a996cee455804dc
adil Zeb <adilzeb776@gmail.com>
2024-05-17 15:19:17 +0500

    String with Different Methods

--- strings.js
+++ strings.js
@@ -19,6 +19,35 @@ console.log(hotelSeat.lastIndexOf('R'));
 
 console.log(hotelSeat.slice(0,-1));
 
+// start With
+console.log(hotelSeat.startsWith('1'));
+ // Ends With
+
+ console.log(hotelSeat.endsWith('R'));
+
+ // charAt 
+
+ console.log(hotelSeat.charAt(1));
+
+ // To upper case 
+
+ console.log(hotelSeat.toLocaleUpperCase());
+
+ // to lower Case
+
+ console.log(hotelSeat.toLocaleLowerCase());
+
+ //Replace with
+
+ console.log(hotelSeat.replace('F',"E"));
+
+ // to remove all spaces 
+
+ console.log(hotelSeat.trim());
+
+ 
+
+
 //function to check weather a seat is front middle or corner
 
 const seat= function(seatData)
@@ -43,5 +72,7 @@ const seat= function(seatData)
 seat('R1F');
 seat('R1M');
 seat('R1C');
+
+
 
 
